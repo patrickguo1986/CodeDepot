7e2fb85fd2ba0920deef2a298000d66c0cfce99c
Minghao Guo <mguo@elembio.com>
2021-10-15 00:08:13 -0700

    convert images to movie

---
+++ images2avi.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+
+image_folder = 'Images'
+video_name = 'Movie.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 0, 6, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
